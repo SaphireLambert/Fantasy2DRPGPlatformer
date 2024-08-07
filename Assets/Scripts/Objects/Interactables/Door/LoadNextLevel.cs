df8a72c1911f33f3151de704c2caa1846afefc94
Saphire Home <107528359+SaphLambert465@users.noreply.github.com>
2024-08-07 12:51:15 +1000

    Remade scene to be more of a level

---
+++ Assets/Scripts/Objects/Interactables/Door/LoadNextLevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject UI;
+
+    private void Start()
+    {
+        UI.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+             UI.SetActive(true);
+        }
+    }
+}
